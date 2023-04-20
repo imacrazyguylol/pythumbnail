af6b121af5d0b8d7a84f99a76618ca90a520ab4e
imacrazyguylol <github@imacrazyguy.xyz>
2023-04-19 22:03:41 -0400

    working on mod combinations

--- imagegen.py
+++ imagegen.py
@@ -1,6 +1,6 @@
 import os, sys, json, shutil, requests
 from ossapi import Ossapi, Score
-from PIL import Image, ImageEnhance, ImageDraw, ImageFont, ImageColor
+from PIL import Image, ImageEnhance, ImageDraw, ImageFont, ImageColor, ImageFilter
 
 # only gets main background from beatmapset
 def __dlImageFromBeatmapID(beatmapset_id):
@@ -30,6 +30,10 @@ def __roundCorners(im, rad):
     alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
     im.putalpha(alpha)
     return im
+
+def __modIcons(score: Score):
+    im = Image.new('RGBA', (556, 132))
+    
     
 def imageGen(score: Score):
     # load font
@@ -38,23 +42,22 @@ def imageGen(score: Score):
     # open background into bkgImage
     beatmapset_id = score.beatmapset.id
     __dlImageFromBeatmapID(beatmapset_id)
-    bkgImage = Image.open('tempbkg.jpg')
+    bkgImage = Image.open('tempbkg.jpg').convert('RGBA')
     
     # open avatar into avatarImage
     user_id = score.user_id
     __dlAvatarFromUID(user_id)
-    avatarImage = Image.open('tempavatar.jpg')
+    avatarImage = Image.open('tempavatar.jpg').convert('RGBA')
     
     # blur background slightly, resize to 1080p, and locally save image, and remove jpg
     bkgImage = bkgImage.resize((1920, 1080))
-    enhance = ImageEnhance.Sharpness(bkgImage)
-    bkgImage = enhance.enhance(0.5) # this is stupid
+    bkgImage = bkgImage.filter(ImageFilter.GaussianBlur(3))
     
     bkgImage.save('tempbkg.png')
     os.remove(os.path.abspath(os.getcwd()) + '/tempbkg.jpg')
     
     # round corners of avatar and locally save image, and remove jpg
-    avatarImage = avatarImage.resize((192, 192))
+    # avatarImage = avatarImage.resize((192, 192)) # only if 720p
     avatarImage = __roundCorners(avatarImage, 35)
     
     avatarImage.save('tempavatar.png')
@@ -64,13 +67,17 @@ def imageGen(score: Score):
     rankIcon = Image.open(f'src/Rankings/ranking-{score.rank.value}.png')
     
     # finally putting together the actual image
-    output = Image.new('RGB', (1920, 1080))
+    output = Image.new('RGBA', (1920, 1080))
     
     output.paste(bkgImage, (0, 0))
-    output.paste(rankIcon, (18, 138))
-    output.paste(avatarImage, (864, 444)) # 1920/2 - 192/2, 1080/2 - 192/2 to get upper right corner of centered image
+    output.paste(rankIcon, (18, 138), rankIcon)
+    output.paste(avatarImage, (832, 362), avatarImage) # 1920/2 - 256/2, 1080/2 - 256/2 to get upper left corner of centered image, then moved up by 50px
     
-    output.save('thumbnail.png')
+    output.save('output/thumbnail.png')
+    output.show()
+    
+    os.remove(os.path.abspath(os.getcwd()) + '/tempbkg.png')
+    os.remove(os.path.abspath(os.getcwd()) + '/tempavatar.png')
 
     # return path to final output
     
